bb994e03da11e5b670b8fc57ff4c449bf1267d9d
Kees Cook <keescook@chromium.org>
2018-10-10 17:18:24 -0700

    LSM: Record LSM name in struct lsm_info
    In preparation for making LSM selections outside of the LSMs, include
    the name of LSMs in struct lsm_info.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Casey Schaufler <casey@schaufler-ca.com>
    Signed-off-by: James Morris <james.morris@microsoft.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- include/linux/lsm_hooks.h
+++ include/linux/lsm_hooks.h
@@ -2003,6 +2003,7 @@ extern void security_add_hooks(struct security_hook_list *hooks, int count,
 				char *lsm);
 
 struct lsm_info {
+	const char *name;	/* Required. */
 	int (*init)(void);	/* Required. */
 };
 
