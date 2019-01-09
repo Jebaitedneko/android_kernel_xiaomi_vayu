b59fe2b12094a908fc5667a5c1bb14a292bd24bd
Alex Naidis <alex.naidis@linux.com>
2019-01-09 10:04:04 -0500

    alarmtimer: Add ndelay for tight loop
    The loop in alarm_cancel() is similar to the ones in the
    timer drivers as it disables IRQs as well.
    
    So we need to add an ndelay() call after the cpu_relax() call
    to avoid potential scheduling delays due to IRQ stalling.
    
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -27,6 +27,7 @@
 #include <linux/posix-timers.h>
 #include <linux/workqueue.h>
 #include <linux/freezer.h>
+#include <linux/delay.h>
 #include <linux/compat.h>
 #include <linux/module.h>
 
@@ -466,6 +467,7 @@ int alarm_cancel(struct alarm *alarm)
 		if (ret >= 0)
 			return ret;
 		cpu_relax();
+		ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
 	}
 }
 EXPORT_SYMBOL_GPL(alarm_cancel);
