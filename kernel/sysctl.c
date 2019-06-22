4c6ef223fe8e19f5e4e2cb59a0a530f353b850bb
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-22 21:37:28 +0900

    mm: merge Samsung mm hacks
    Forward-port to v4.14
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>
    Change-Id: I5ea9f3260f293e3a80e45fdb12a6eb738b3ab1e2

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -138,6 +138,9 @@ static int one_thousand = 1000;
 #ifdef CONFIG_SCHED_WALT
 static int two_million = 2000000;
 #endif
+#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
+static int max_swappiness = 200;
+#endif
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -1554,7 +1557,11 @@ static struct ctl_table vm_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
+#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
+		.extra2         = &max_swappiness,
+#else
 		.extra2		= &one_hundred,
+#endif
 	},
 	{
 		.procname       = "want_old_faultaround_pte",
