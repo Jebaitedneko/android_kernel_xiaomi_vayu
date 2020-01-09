14848e3543d82dfd3c876bb46873b1c68165b7e0
Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
2020-01-09 16:40:47 +0530

    selinux: move ibpkeys code under CONFIG_SECURITY_INFINIBAND.
    Move cache based  pkey sid  retrieval code which was added
    with commit "409dcf31" under CONFIG_SECURITY_INFINIBAND.
    As its  going to alloc a new cache which impacts
    low RAM devices which was enabled by default.
    
    Suggested-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Ravi Kumar Siddojigari <rsiddoji@codeaurora.org>
    [PM: checkpatch.pl cleanups, fixed capitalization in the description]
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- security/selinux/include/ibpkey.h
+++ security/selinux/include/ibpkey.h
@@ -26,8 +26,18 @@
 
 #ifdef CONFIG_SECURITY_INFINIBAND
 void sel_ib_pkey_flush(void);
-
 int sel_ib_pkey_sid(u64 subnet_prefix, u16 pkey, u32 *sid);
+#else
+static inline void sel_ib_pkey_flush(void)
+{
+	return;
+}
+static inline int sel_ib_pkey_sid(u64 subnet_prefix, u16 pkey, u32 *sid)
+{
+	*sid = SECINITSID_UNLABELED;
+	return 0;
+}
+#endif
 
 #else
 
