ffa7c9d7d839a985e9fe57dc3d0a46739c7626fe
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-13 04:57:04 +0900

    mm: compaction: fix compilation on !CONFIG_ZSWAP
    Change-Id: I69f1fe1f1f2b16c5c23bb1494a0b6249f9b2c8af
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1910,7 +1910,11 @@ static void compact_node(int nid)
 	}
 }
 
+#ifdef CONFIG_ZSWAP
 extern void zswap_compact(void);
+#else
+static inline void zswap_compact(void) {}
+#endif
 /* Compact all nodes in the system */
 static void compact_nodes(void)
 {
