60ba1804a08814072a90469514e28e8c9f7a908b
Yaroslav Furman <yaro330@gmail.com>
2020-11-21 22:39:54 +0300

    input/ts: nt36xxx: Remove MTK parts
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.h
+++ drivers/input/touchscreen/nt36672c/nt36xxx.h
@@ -34,14 +34,6 @@
 
 #include "nt36xxx_mem_map.h"
 
-#ifdef CONFIG_MTK_SPI
-#include "mtk_spi.h"
-#endif
-
-#ifdef CONFIG_SPI_MT65XX
-#include <linux/platform_data/spi-mt65xx.h>
-#endif
-
 #define FW_HISTORY_SIZE	128
 /*Lock down info size*/
 #define NVT_LOCKDOWN_SIZE			8
@@ -168,12 +160,6 @@ struct nvt_ts_data {
 	uint8_t *xbuf;
 	struct mutex xbuf_lock;
 	bool irq_enabled;
-#ifdef CONFIG_MTK_SPI
-	struct mt_chip_conf spi_ctrl;
-#endif
-#ifdef CONFIG_SPI_MT65XX
-    struct mtk_chip_config spi_ctrl;
-#endif
 	struct pinctrl *ts_pinctrl;
 	struct pinctrl_state *pinctrl_state_active;
 	struct pinctrl_state *pinctrl_state_suspend;
