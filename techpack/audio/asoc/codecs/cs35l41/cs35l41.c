e794b62e595dc092d964b9e3213eecb18452bbbc
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-09 16:15:48 +0530

    techpack: codecs: cs35l41: rename spk_id_get to avoid linker errors

--- techpack/audio/asoc/codecs/cs35l41/cs35l41.c
+++ techpack/audio/asoc/codecs/cs35l41/cs35l41.c
@@ -208,6 +208,34 @@ static int cs35l41_halo_booted_put(struct snd_kcontrol *kcontrol,
 	return 0;
 }
 
+int cs35l41_spk_id_get(struct device_node *np)
+{
+	int id;
+	int state;
+
+	state = spk_id_get_pin_3state(np);
+	if (state < 0) {
+		pr_err("%s: Can not get id pin state, %d\n", __func__, state);
+		return VENDOR_ID_NONE;
+	}
+
+	switch (state) {
+	case PIN_PULL_DOWN:
+		id = VENDOR_ID_AAC;
+		break;
+	case PIN_PULL_UP:
+		id = VENDOR_ID_UNKNOWN;
+		break;
+	case PIN_FLOAT:
+		id = VENDOR_ID_GOER;
+		break;
+	default:
+		id = VENDOR_ID_UNKNOWN;
+		break;
+	}
+	return id;
+}
+
 static int vendor_id_get(struct snd_kcontrol *kcontrol,
 					struct snd_ctl_elem_value *ucontrol)
 {
@@ -217,7 +245,7 @@ static int vendor_id_get(struct snd_kcontrol *kcontrol,
 	ucontrol->value.integer.value[0] = VENDOR_ID_NONE;
 
 	if (cs35l41->pdata.spk_id_gpio_p)
-		ucontrol->value.integer.value[0] = spk_id_get(cs35l41->pdata.spk_id_gpio_p);
+		ucontrol->value.integer.value[0] = cs35l41_spk_id_get(cs35l41->pdata.spk_id_gpio_p);
 
 	return 0;
 }
@@ -1890,34 +1918,6 @@ static struct snd_soc_codec_driver soc_codec_dev_cs35l41 = {
 	.idle_bias_off = true,
 };
 
-int spk_id_get(struct device_node *np)
-{
-	int id;
-	int state;
-
-	state = spk_id_get_pin_3state(np);
-	if (state < 0) {
-		pr_err("%s: Can not get id pin state, %d\n", __func__, state);
-		return VENDOR_ID_NONE;
-	}
-
-	switch (state) {
-	case PIN_PULL_DOWN:
-		id = VENDOR_ID_AAC;
-		break;
-	case PIN_PULL_UP:
-		id = VENDOR_ID_UNKNOWN;
-		break;
-	case PIN_FLOAT:
-		id = VENDOR_ID_GOER;
-		break;
-	default:
-		id = VENDOR_ID_UNKNOWN;
-		break;
-	}
-	return id;
-}
-
 static int cs35l41_handle_of_data(struct device *dev,
 				  struct cs35l41_platform_data *pdata,
 				  struct cs35l41_private *cs35l41)
@@ -2026,7 +2026,7 @@ static int cs35l41_handle_of_data(struct device *dev,
 		"cirrus,spk-id-pin", np->full_name);
 		pdata->mnSpkType = VENDOR_ID_NONE;
 	} else {
-		pdata->mnSpkType = spk_id_get(pdata->spk_id_gpio_p);
+		pdata->mnSpkType = cs35l41_spk_id_get(pdata->spk_id_gpio_p);
 	}
 	dev_info(dev, "spk is is %d", pdata->mnSpkType);
 
