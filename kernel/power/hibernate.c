ed226c645f13937d6f7bb84ee59057a4074b5efe
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-19 02:32:09 +0530

    kernel: power: omit trace markers and logspam

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -337,7 +337,6 @@ static int create_image(int platform_mode)
  Platform_finish:
 	platform_finish(platform_mode);
 
-	place_marker("M - Hibernation: start device resume");
 	dpm_resume_start(in_suspend ?
 		(error ? PMSG_RECOVER : PMSG_THAW) : PMSG_RESTORE);
 
@@ -413,7 +412,6 @@ int hibernation_snapshot(int platform_mode)
 
 	resume_console();
 	dpm_complete(msg);
-	place_marker("M - Hibernation: end device resume");
  Close:
 	platform_end(platform_mode);
 	return error;
@@ -705,7 +703,6 @@ int hibernate(void)
 		goto Unlock;
 	}
 
-	pr_info("hibernation entry\n");
 	pm_prepare_console();
 	error = __pm_notifier_call_chain(PM_HIBERNATION_PREPARE, -1, &nr_calls);
 	if (error) {
@@ -713,9 +710,7 @@ int hibernate(void)
 		goto Exit;
 	}
 
-	pr_info("Syncing filesystems ... \n");
 	sys_sync();
-	pr_info("done.\n");
 
 	error = freeze_processes();
 	if (error)
@@ -753,7 +748,6 @@ int hibernate(void)
 		in_suspend = 0;
 		pm_restore_gfp_mask();
 	} else {
-		place_marker("M - PM: Image restored!");
 		pm_pr_dbg("Image restored successfully.\n");
 	}
 
@@ -768,7 +762,6 @@ int hibernate(void)
 			error = load_image_and_restore();
 	}
 	thaw_processes();
-	place_marker("M - Hibernation: processes thaw done");
 
 	/* Don't bother checking whether freezer_test_done is true */
 	freezer_test_done = false;
@@ -778,8 +771,6 @@ int hibernate(void)
 	atomic_inc(&snapshot_device_available);
  Unlock:
 	unlock_system_sleep();
-	place_marker("M - PM: Hibernation Exit!");
-	pr_info("hibernation exit\n");
 
 	return error;
 }
@@ -908,7 +899,6 @@ static int software_resume(void)
 
 	error = load_image_and_restore();
 	thaw_processes();
-	place_marker("M - PM: Thaw processes completed!");
  Finish:
 	__pm_notifier_call_chain(PM_POST_RESTORE, nr_calls, NULL);
 	pm_restore_console();
