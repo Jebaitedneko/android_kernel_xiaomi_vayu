e2fe542fe0c139dbb50acd2d8d64e7b5f370c7a9
Ard Biesheuvel <ardb@kernel.org>
2021-07-23 01:59:34 +0530

    crypto: arm64/sha1-ce - simplify NEON yield
    Instead of calling into kernel_neon_end() and kernel_neon_begin() (and
    potentially into schedule()) from the assembler code when running in
    task mode and a reschedule is pending, perform only the preempt count
    check in assembler, but simply return early in this case, and let the C
    code deal with the consequences.
    
    This reverts commit 7df8d164753e6e6f229b72767595072bc6a71f48.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- arch/arm64/crypto/sha1-ce-glue.c
+++ arch/arm64/crypto/sha1-ce-glue.c
@@ -32,8 +32,8 @@ struct sha1_ce_state {
 extern const u32 sha1_ce_offsetof_count;
 extern const u32 sha1_ce_offsetof_finalize;
 
-asmlinkage void sha1_ce_transform(struct sha1_ce_state *sst, u8 const *src,
-				  int blocks);
+asmlinkage int sha1_ce_transform(struct sha1_ce_state *sst, u8 const *src,
+				 int blocks);
 #ifdef CONFIG_CFI_CLANG
 static inline void __cfi_sha1_ce_transform(struct sha1_state *sst,
 					   u8 const *src, int blocks)
@@ -48,6 +48,16 @@ static void __sha1_ce_transform(struct sha1_state *sst, u8 const *src,
 {
 	sha1_ce_transform(container_of(sst, struct sha1_ce_state, sst), src,
 			  blocks);
+	while (blocks) {
+		int rem;
+
+		kernel_neon_begin();
+		rem = sha1_ce_transform(container_of(sst, struct sha1_ce_state,
+						     sst), src, blocks);
+		kernel_neon_end();
+		src += (blocks - rem) * SHA1_BLOCK_SIZE;
+		blocks = rem;
+	}
 }
 
 const u32 sha1_ce_offsetof_count = offsetof(struct sha1_ce_state, sst.count);
@@ -62,9 +72,7 @@ static int sha1_ce_update(struct shash_desc *desc, const u8 *data,
 		return crypto_sha1_update(desc, data, len);
 
 	sctx->finalize = 0;
-	kernel_neon_begin();
 	sha1_base_do_update(desc, data, len, __sha1_ce_transform);
-	kernel_neon_end();
 
 	return 0;
 }
@@ -84,11 +92,9 @@ static int sha1_ce_finup(struct shash_desc *desc, const u8 *data,
 	 */
 	sctx->finalize = finalize;
 
-	kernel_neon_begin();
 	sha1_base_do_update(desc, data, len, __sha1_ce_transform);
 	if (!finalize)
 		sha1_base_do_finalize(desc, __sha1_ce_transform);
-	kernel_neon_end();
 	return sha1_base_finish(desc, out);
 }
 
@@ -100,9 +106,7 @@ static int sha1_ce_final(struct shash_desc *desc, u8 *out)
 		return crypto_sha1_finup(desc, NULL, 0, out);
 
 	sctx->finalize = 0;
-	kernel_neon_begin();
 	sha1_base_do_finalize(desc, __sha1_ce_transform);
-	kernel_neon_end();
 	return sha1_base_finish(desc, out);
 }
 
