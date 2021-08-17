842b5cfd762313fb399e2b2142bc13e190ffca24
Park Ju Hyung <qkrwngud825@gmail.com>
2021-08-17 14:03:52 +0700

    treewide: remove remaining _no_log() usage
    sed -i -e 's/_no_log//g' $(git grep -l _no_log | tr '\n' ' ')
    
    and manually fix drivers/clk/qcom/clk-cpu-osm.c.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [fiqri19102002: Fixes clk-cpu-osm-660.c]
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/soc/qcom/dcc_v2.c
+++ drivers/soc/qcom/dcc_v2.c
@@ -198,7 +198,7 @@ static void dcc_sram_memset(const struct device *dev, void __iomem *dst,
 	}
 
 	while (count >= 4) {
-		__raw_writel_no_log(qc, dst);
+		__raw_writel(qc, dst);
 		dst += 4;
 		count -= 4;
 	}
@@ -214,7 +214,7 @@ static int dcc_sram_memcpy(void *to, const void __iomem *from,
 	}
 
 	while (count >= 4) {
-		*(unsigned int *)to = __raw_readl_no_log(from);
+		*(unsigned int *)to = __raw_readl(from);
 		to += 4;
 		from += 4;
 		count -= 4;
@@ -1939,7 +1939,7 @@ static int dcc_v2_restore(struct device *dev)
 	data = drvdata->sram_save_state;
 
 	for (i = 0; i < drvdata->ram_size / 4; i++)
-		__raw_writel_no_log(data[i],
+		__raw_writel(data[i],
 					drvdata->ram_base + (i * 4));
 
 	state = drvdata->reg_save_state;
