1da829f46aaeb737571a1a75d1fcfb326c07afa2
Jebaitedneko <Jebaitedneko@gmail.com>
2021-11-10 07:23:27 +0530

    input: touchscreen: nt36672c: set esd checking period to 125ms
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- drivers/input/touchscreen/nt36672c/nt36xxx.h
+++ drivers/input/touchscreen/nt36672c/nt36xxx.h
@@ -105,7 +105,7 @@
 
 /* ---ESD Protect.--- */
 #define NVT_TOUCH_ESD_PROTECT 1
-#define NVT_TOUCH_ESD_CHECK_PERIOD 1500	/* ms */
+#define NVT_TOUCH_ESD_CHECK_PERIOD 125		/* ms */
 #define NVT_TOUCH_WDT_RECOVERY 1
 #define NVT_TOUCH_ESD_DISP_RECOVERY 1
 
