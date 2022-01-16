a5e2b2faa81183306feef966fb80eee197f97607
Phanindra Babu Pabba <pabba@codeaurora.org>
2022-01-16 16:22:53 +0530

    Avoid fatal error if ICE registers are not defined
    Disabling crypto configurations if ufs_ice/ cmdq_ice
    registers not defined for the given storage controller
    and avoid to consider it as fatal error.
    
    Change-Id: I2eefe69a5021ba3dcd0ce90bb4382228e60e6f6d
    Signed-off-by: Phanindra Babu Pabba <pabba@codeaurora.org>

--- drivers/mmc/host/cmdq_hci-crypto-qti.c
+++ drivers/mmc/host/cmdq_hci-crypto-qti.c
@@ -363,7 +363,8 @@ int cmdq_crypto_qti_init_crypto(struct cmdq_host *host,
 	if (!cmdq_ice_memres) {
 		pr_debug("%s ICE not supported\n", __func__);
 		host->icemmio = NULL;
-		return PTR_ERR(cmdq_ice_memres);
+		host->caps &= ~CMDQ_CAP_CRYPTO_SUPPORT;
+		return err;
 	}
 
 	host->icemmio = devm_ioremap(&msm_host->pdev->dev,
