84481f5cb7bf9fd3e7b491dbe2f55f5683f8d12f
Sultan Alsawaf <sultan@kerneltoast.com>
2022-02-09 01:09:17 -0800

    Revert "workqueue: fix possible livelock with concurrent mod_delayed_work()"
    This reverts commit 52eea4fa0264efdfbc758f784cb8a87c789403a9.
    
    The PREEMPT_RT patchset has its own fix for this.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -48,8 +48,6 @@
 #include <linux/nodemask.h>
 #include <linux/moduleparam.h>
 #include <linux/uaccess.h>
-#include <linux/bug.h>
-#include <linux/delay.h>
 #include <linux/nmi.h>
 #include <linux/kvm_para.h>
 
@@ -1299,12 +1297,6 @@ fail:
 	if (work_is_canceling(work))
 		return -ENOENT;
 	cpu_relax();
-	/*
-	 * The queueing is in progress in another context. If we keep
-	 * taking the pool->lock in a busy loop, the other context may
-	 * never get the lock. Give 1 usec delay to avoid this contention.
-	 */
-	udelay(1);
 	return -EAGAIN;
 }
 
