e55174d0b5c13183a9f977cc7cee0930e5957507
Jebaitedneko <Jebaitedneko@gmail.com>
2022-02-10 22:29:12 +0530

    input: fingerprint: goodix_ta_vayu: cleanup & rewrite v2
    * rework netlink sender logic
    * rework irq handler
    * move common function calls under gf_{setup,cleanup}
    * fixup mutexes and function call ordering
    * normalize variable names
    * move defines just before they're used
    * move to spdx license identifier
    * fixup module authorship typo
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- drivers/input/fingerprint/goodix_ta_vayu/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta_vayu/gf_spi.c
@@ -1,17 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * TEE driver for goodix fingerprint sensor
  * Copyright (C) 2016 Goodix
- * Copyright (C) 2021 Jebaitedneko <jebaitedneko@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (C) 2022 Jebaitedneko <Jebaitedneko@gmail.com>
  */
 
 #include <linux/delay.h>
@@ -35,173 +26,128 @@
 #include <net/netlink.h>
 #include <net/sock.h>
 
-#define GF_IOC_MAGIC 'g'
-#define GF_IOC_INIT _IOR(GF_IOC_MAGIC, 0, uint8_t)
-#define GF_IOC_RESET _IO(GF_IOC_MAGIC, 2)
-#define GF_IOC_ENABLE_IRQ _IO(GF_IOC_MAGIC, 3)
-#define GF_IOC_DISABLE_IRQ _IO(GF_IOC_MAGIC, 4)
-
-#define GF_SPIDEV_NAME "goodix,fingerprint"
-#define GF_DEV_NAME "goodix_fp"
-#define GF_INPUT_NAME "uinput-goodix"
-#define CHRD_DRIVER_NAME "goodix_fp_spi"
-#define CLASS_NAME "goodix_fp"
-#define N_SPI_MINORS 256
-
-#define GF_NET_EVENT_IRQ 1
-#define NETLINK_TEST 25
-#define MAX_MSGSIZE 16
-
-struct gf_dev {
+static struct gf_dev {
 	dev_t devt;
 	struct list_head device_entry;
 	struct platform_device *spi;
 	struct input_dev *input;
 	struct regulator *vreg;
-	unsigned users;
-	signed irq_gpio;
-	signed reset_gpio;
-	int irq;
-	int irq_enabled;
-};
+	signed irq_gpio, rst_gpio;
+	int irq, irq_enabled;
+} gf;
 
-static int SPIDEV_MAJOR;
-static DECLARE_BITMAP(minors, N_SPI_MINORS);
-static LIST_HEAD(device_list);
-static DEFINE_MUTEX(gf_lock);
-static struct gf_dev gf;
-
-static int pid = -1;
+#define MAX_MSGSIZE 2
 static struct sock *nl_sk = NULL;
-
-static inline void sendnlmsg(char *message) {
-	struct sk_buff *skb_1;
-	struct nlmsghdr *nlh;
-	int len = NLMSG_SPACE(MAX_MSGSIZE);
-	int slen = 0;
-	if (!message || !nl_sk)
-		return;
-	skb_1 = alloc_skb(len, GFP_KERNEL);
-	if (!skb_1)
-		return;
-	slen = strlen(message);
-	nlh = nlmsg_put(skb_1, 0, 0, 0, MAX_MSGSIZE, 0);
-	NETLINK_CB(skb_1).portid = 0;
-	NETLINK_CB(skb_1).dst_group = 0;
-	message[slen] = '\0';
-	memcpy(NLMSG_DATA(nlh), message, slen + 1);
+static int pid = -1;
+static inline int sendnlmsg(void) {
+	struct sk_buff *skb_1 = alloc_skb(NLMSG_SPACE(MAX_MSGSIZE), GFP_KERNEL);
+	struct nlmsghdr *nlh = nlmsg_put(skb_1, 0, 0, 0, MAX_MSGSIZE, 0);
+	char msg[MAX_MSGSIZE] = { 1, 0 };
+	memcpy(NLMSG_DATA(nlh), msg, MAX_MSGSIZE);
+	NETLINK_CB(skb_1).portid = NETLINK_CB(skb_1).dst_group = 0;
 	netlink_unicast(nl_sk, skb_1, pid, MSG_DONTWAIT);
+	return 1;
 }
 
 static inline void nl_data_ready(struct sk_buff *__skb) {
-	struct sk_buff *skb;
-	struct nlmsghdr *nlh;
-	char str[MAX_MSGSIZE];
-	skb = skb_get(__skb);
-	if (skb->len >= NLMSG_SPACE(0)) {
-		nlh = nlmsg_hdr(skb);
-		memcpy(str, NLMSG_DATA(nlh), sizeof(str));
-		pid = nlh->nlmsg_pid;
-		kfree_skb(skb);
-	}
+	struct sk_buff *skb = skb_get(__skb);
+	struct nlmsghdr *nlh = nlmsg_hdr(skb);
+	pid = nlh->nlmsg_pid;
+	kfree_skb(skb);
 }
 
+#define NETLINK_TEST 25
 static inline void netlink_init(void) {
 	struct netlink_kernel_cfg netlink_cfg;
-	netlink_cfg.groups = 0;
-	netlink_cfg.flags = 0;
+	netlink_cfg.groups = netlink_cfg.flags = 0;
 	netlink_cfg.input = nl_data_ready;
 	netlink_cfg.cb_mutex = NULL;
 	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST, &netlink_cfg);
 }
 
 static inline void netlink_exit(void) {
-	if (!nl_sk)
-		return;
 	netlink_kernel_release(nl_sk);
 	nl_sk = NULL;
-}
-
-static inline int gf_parse_dts(struct gf_dev *gf_dev) {
-	gf_dev->reset_gpio =
-		of_get_named_gpio(gf_dev->spi->dev.of_node, "goodix,gpio-reset", 0);
-	gpio_request(gf_dev->reset_gpio, "gpio-reset");
-	gpio_direction_output(gf_dev->reset_gpio, 1);
-	gf_dev->irq_gpio =
-		of_get_named_gpio(gf_dev->spi->dev.of_node, "goodix,gpio-irq", 0);
-	gpio_request(gf_dev->irq_gpio, "gpio-irq");
-	gpio_direction_input(gf_dev->irq_gpio);
-	return 0;
-}
-
-static inline void gf_cleanup(struct gf_dev *gf_dev) {
-	if (gpio_is_valid(gf_dev->irq_gpio))
-		gpio_free(gf_dev->irq_gpio);
-	if (gpio_is_valid(gf_dev->reset_gpio))
-		gpio_free(gf_dev->reset_gpio);
-}
-
-static inline irqreturn_t gf_irq(int irq, void *handle) {
-	char msg[2] = {0x0};
-	msg[0] = GF_NET_EVENT_IRQ;
-	sendnlmsg(msg);
-	return IRQ_HANDLED;
 }
 
 static inline void irq_switch(struct gf_dev *gf_dev, int status) {
 	if (status) {
 		if (!gf_dev->irq_enabled) {
 			enable_irq_wake(gf_dev->irq);
-			irq_set_affinity(gf_dev->irq, cpumask_of(7));
+			gf_dev->irq_enabled = 1;
 		}
-		gf_dev->irq_enabled = 1;
 	} else {
-		if (gf_dev->irq_enabled)
-			disable_irq(gf_dev->irq);
-		gf_dev->irq_enabled = 0;
+		if (gf_dev->irq_enabled) {
+			disable_irq_wake(gf_dev->irq);
+			gf_dev->irq_enabled = 0;
+		}
+	}
+}
+
+static inline irqreturn_t gf_irq(int irq, void *handle) {
+	return sendnlmsg();
+}
+
+static inline void gf_setup(struct gf_dev *gf_dev) {
+	gf_dev->rst_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,
+		"goodix,gpio-reset", 0);
+	gpio_request(gf_dev->rst_gpio, "gpio-reset");
+	gpio_direction_output(gf_dev->rst_gpio, 1);
+	gf_dev->irq_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,
+		"goodix,gpio-irq", 0);
+	gpio_request(gf_dev->irq_gpio, "gpio-irq");
+	gpio_direction_input(gf_dev->irq_gpio);
+	gf_dev->irq = gpio_to_irq(gf_dev->irq_gpio);
+	if (request_threaded_irq(gf_dev->irq, NULL, gf_irq,
+			IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_NO_SUSPEND | IRQF_FORCE_RESUME,
+				"gf", gf_dev))
+		irq_switch(gf_dev, 1);
+	gf_dev->vreg = regulator_get(NULL, "pm8150_l17");
+	if (!regulator_is_enabled(gf_dev->vreg)) {
+		regulator_set_voltage(gf_dev->vreg, 3000000, 3000000);
+		if (regulator_enable(gf_dev->vreg))
+			return;
+	}
+	return;
+}
+
+static inline void gf_cleanup(struct gf_dev *gf_dev) {
+	if (gf_dev->irq_enabled) {
+		irq_switch(gf_dev, 0);
+		free_irq(gf_dev->irq, gf_dev);
+	}
+	if (gpio_is_valid(gf_dev->irq_gpio))
+		gpio_free(gf_dev->irq_gpio);
+	if (gpio_is_valid(gf_dev->rst_gpio))
+		gpio_free(gf_dev->rst_gpio);
+	if (regulator_is_enabled(gf_dev->vreg)) {
+		regulator_disable(gf_dev->vreg);
+		regulator_put(gf_dev->vreg);
+		gf_dev->vreg = NULL;
 	}
 }
 
 static inline void gpio_reset(struct gf_dev *gf_dev) {
-	gpio_direction_output(gf_dev->reset_gpio, 1);
-	gpio_set_value(gf_dev->reset_gpio, 0);
+	gpio_direction_output(gf_dev->rst_gpio, 1);
+	gpio_set_value(gf_dev->rst_gpio, 0);
 	mdelay(3);
-	gpio_set_value(gf_dev->reset_gpio, 1);
+	gpio_set_value(gf_dev->rst_gpio, 1);
 	mdelay(3);
 }
 
-static inline int irq_setup(struct gf_dev *gf_dev) {
-	int status;
-	gf_dev->irq = gpio_to_irq(gf_dev->irq_gpio);
-	status =
-		request_threaded_irq(gf_dev->irq, NULL, gf_irq,
-							 IRQF_TRIGGER_RISING | IRQF_ONESHOT, "gf", gf_dev);
-	if (status)
-		return status;
-	irq_switch(gf_dev, 1);
-	return status;
-}
-
-static inline void irq_cleanup(struct gf_dev *gf_dev) {
-	if (gf_dev->irq_enabled)
-		disable_irq_wake(gf_dev->irq);
-	disable_irq(gf_dev->irq);
-	free_irq(gf_dev->irq, gf_dev);
-	gf_dev->irq_enabled = 0;
-}
-
+#define GF_IOC_MAGIC 'g'
+#define GF_IOC_INIT _IOR(GF_IOC_MAGIC, 0, uint8_t)
+#define GF_IOC_RESET _IO(GF_IOC_MAGIC, 2)
+#define GF_IOC_ENABLE_IRQ _IO(GF_IOC_MAGIC, 3)
+#define GF_IOC_DISABLE_IRQ _IO(GF_IOC_MAGIC, 4)
 static inline long gf_ioctl(struct file *filp, unsigned int cmd,
 							unsigned long arg) {
 	struct gf_dev *gf_dev = &gf;
-	int retval = 0;
 	u8 netlink_route = NETLINK_TEST;
 	switch (cmd) {
 	case GF_IOC_INIT:
-		if (copy_to_user((void __user *)arg, (void *)&netlink_route,
-						 sizeof(u8))) {
-			retval = -EFAULT;
+		if (copy_to_user((void __user *)arg, (void *)&netlink_route, sizeof(u8)))
 			break;
-		}
 		break;
 	case GF_IOC_DISABLE_IRQ:
 		irq_switch(gf_dev, 0);
@@ -215,60 +161,29 @@ static inline long gf_ioctl(struct file *filp, unsigned int cmd,
 	default:
 		break;
 	}
-	return retval;
+	return 0;
 }
 
+static DEFINE_MUTEX(gf_lock);
 static inline int gf_open(struct inode *inode, struct file *filp) {
 	struct gf_dev *gf_dev = &gf;
-	int status = -ENXIO;
 	mutex_lock(&gf_lock);
-	list_for_each_entry(gf_dev, &device_list, device_entry) {
-		if (gf_dev->devt == inode->i_rdev) {
-			status = 0;
-			break;
-		}
-	}
-	if (status == 0) {
-		gf_dev->users++;
-		filp->private_data = gf_dev;
-		nonseekable_open(inode, filp);
-		if (gf_dev->users == 1) {
-			status = gf_parse_dts(gf_dev);
-			if (status)
-				return status;
-			status = irq_setup(gf_dev);
-			if (status)
-				gf_cleanup(gf_dev);
-		}
-		gpio_reset(gf_dev);
-	}
-	gf_dev->vreg = regulator_get(NULL, "pm8150_l17");
-	if (!regulator_is_enabled(gf_dev->vreg)) {
-		regulator_set_voltage(gf_dev->vreg, 3000000, 3000000);
-		regulator_enable(gf_dev->vreg);
-	}
+	filp->private_data = gf_dev;
+	nonseekable_open(inode, filp);
+	gf_setup(gf_dev);
+	gpio_reset(gf_dev);
 	mutex_unlock(&gf_lock);
-	return status;
+	return 0;
 }
 
 static inline int gf_release(struct inode *inode, struct file *filp) {
 	struct gf_dev *gf_dev = &gf;
-	int status = 0;
 	mutex_lock(&gf_lock);
 	gf_dev = filp->private_data;
 	filp->private_data = NULL;
-	gf_dev->users--;
-	if (!gf_dev->users) {
-		irq_cleanup(gf_dev);
-		gf_cleanup(gf_dev);
-	}
-	if (regulator_is_enabled(gf_dev->vreg)) {
-		regulator_disable(gf_dev->vreg);
-		regulator_put(gf_dev->vreg);
-		gf_dev->vreg = NULL;
-	}
+	gf_cleanup(gf_dev);
 	mutex_unlock(&gf_lock);
-	return status;
+	return 0;
 }
 
 static const struct file_operations gf_fops = {
@@ -279,100 +194,66 @@ static const struct file_operations gf_fops = {
 };
 
 static struct class *gf_class;
+#define N_SPI_MINORS 256
+static DECLARE_BITMAP(minors, N_SPI_MINORS);
+static LIST_HEAD(device_list);
+static int SPIDEV_MAJOR;
+#define GF_DEV_NAME "goodix_fp"
+#define GF_INPUT_NAME "uinput-goodix"
 static inline int gf_probe(struct platform_device *pdev) {
 	struct gf_dev *gf_dev = &gf;
-	int status = -EINVAL;
-	unsigned long minor;
+	unsigned long minor = find_first_zero_bit(minors, N_SPI_MINORS);
 	INIT_LIST_HEAD(&gf_dev->device_entry);
 	gf_dev->spi = pdev;
-	gf_dev->irq_gpio = -EINVAL;
-	gf_dev->reset_gpio = -EINVAL;
+	gf_dev->irq_gpio = gf_dev->rst_gpio = -EINVAL;
+	gf_dev->devt = MKDEV(SPIDEV_MAJOR, minor);
 	mutex_lock(&gf_lock);
-	minor = find_first_zero_bit(minors, N_SPI_MINORS);
-	if (minor < N_SPI_MINORS) {
-		struct device *dev;
-		gf_dev->devt = MKDEV(SPIDEV_MAJOR, minor);
-		dev = device_create(gf_class, &gf_dev->spi->dev, gf_dev->devt, gf_dev,
-							GF_DEV_NAME);
-		status = IS_ERR(dev) ? PTR_ERR(dev) : 0;
-	} else {
-		status = -ENODEV;
-		return status;
-	}
+	device_create(gf_class, &gf_dev->spi->dev, gf_dev->devt, gf_dev,
+					GF_DEV_NAME);
+	set_bit(minor, minors);
+	list_add(&gf_dev->device_entry, &device_list);
 	mutex_unlock(&gf_lock);
-	if (status == 0) {
-		set_bit(minor, minors);
-		list_add(&gf_dev->device_entry, &device_list);
-	} else {
-		gf_dev->devt = 0;
-		return status;
-	}
 	gf_dev->input = input_allocate_device();
-	if (gf_dev->input == NULL) {
-		status = -ENOMEM;
-		if (gf_dev->input != NULL)
-			input_free_device(gf_dev->input);
-	}
 	gf_dev->input->name = GF_INPUT_NAME;
-	status = input_register_device(gf_dev->input);
-	if (status) {
-		if (gf_dev->devt != 0) {
-			mutex_lock(&gf_lock);
-			list_del(&gf_dev->device_entry);
-			device_destroy(gf_class, gf_dev->devt);
-			clear_bit(MINOR(gf_dev->devt), minors);
-			mutex_unlock(&gf_lock);
-		}
-	}
-	return status;
+	return input_register_device(gf_dev->input);
 }
 
 static inline int gf_remove(struct platform_device *pdev) {
 	struct gf_dev *gf_dev = &gf;
-	if (gf_dev->input)
+	if (gf_dev->input) {
 		input_unregister_device(gf_dev->input);
-	input_free_device(gf_dev->input);
+		input_free_device(gf_dev->input);
+	}
 	mutex_lock(&gf_lock);
 	list_del(&gf_dev->device_entry);
-	device_destroy(gf_class, gf_dev->devt);
 	clear_bit(MINOR(gf_dev->devt), minors);
+	device_destroy(gf_class, gf_dev->devt);
 	mutex_unlock(&gf_lock);
 	return 0;
 }
 
 static const struct of_device_id gx_match_table[] = {
-	{.compatible = GF_SPIDEV_NAME},
+	{.compatible = "goodix,fingerprint"},
 	{},
 };
 
 static struct platform_driver gf_driver = {
-	.driver =
-		{
-			.name = GF_DEV_NAME,
-			.owner = THIS_MODULE,
-			.of_match_table = gx_match_table,
-		},
+	.driver = {
+		.name = GF_DEV_NAME,
+		.owner = THIS_MODULE,
+		.of_match_table = gx_match_table,
+	},
 	.probe = gf_probe,
 	.remove = gf_remove,
 };
 
+#define CHRD_DRIVER_NAME "goodix_fp_spi"
+#define CLASS_NAME "goodix_fp"
 static inline int __init gf_init(void) {
-	int status;
-	BUILD_BUG_ON(N_SPI_MINORS > 256);
-	status = register_chrdev(SPIDEV_MAJOR, CHRD_DRIVER_NAME, &gf_fops);
-	if (status < 0)
-		return status;
+	int status = register_chrdev(SPIDEV_MAJOR, CHRD_DRIVER_NAME, &gf_fops);
 	SPIDEV_MAJOR = status;
 	gf_class = class_create(THIS_MODULE, CLASS_NAME);
-	if (IS_ERR(gf_class)) {
-		unregister_chrdev(SPIDEV_MAJOR, gf_driver.driver.name);
-		return PTR_ERR(gf_class);
-	}
-	status = platform_driver_register(&gf_driver);
-	if (status < 0) {
-		class_destroy(gf_class);
-		unregister_chrdev(SPIDEV_MAJOR, gf_driver.driver.name);
-	}
+	platform_driver_register(&gf_driver);
 	netlink_init();
 	return 0;
 }
@@ -388,6 +269,6 @@ module_exit(gf_exit);
 
 MODULE_AUTHOR("Jiangtao Yi, <yijiangtao@goodix.com>");
 MODULE_AUTHOR("Jandy Gou, <gouqingsong@goodix.com>");
-MODULE_AUTHOR("Jebaitedneko, <jebaitedneko@gmail.com");
+MODULE_AUTHOR("Jebaitedneko, <Jebaitedneko@gmail.com>");
 MODULE_DESCRIPTION("goodix fingerprint sensor device driver");
 MODULE_LICENSE("GPL");
