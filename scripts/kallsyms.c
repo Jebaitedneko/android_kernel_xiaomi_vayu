528d22c1cf8e70810f7866b85e1b7a1cd52caa41
Tung Vu <vutung.ct@gmail.com>
2022-03-12 22:43:06 +0530

    Kbuild: fix kallsyms failure when building with GCC LTO for arm64
    Fix error "kallsyms failure: relative symbol value 0xffffff8008080000 out of range in relative mode"
    
    Co-authored-by: Jebaitedneko <Jebaitedneko@gmail.com>
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- scripts/kallsyms.c
+++ scripts/kallsyms.c
@@ -114,13 +114,15 @@ static int check_symbol_range(const char *sym, unsigned long long addr,
 static int read_symbol(FILE *in, struct sym_entry *s)
 {
 	char str[500];
+	char buf[LINE_MAX];
 	char *sym, stype;
 	int rc;
 
-	rc = fscanf(in, "%llx %c %499s\n", &s->addr, &stype, str);
-	if (rc != 3) {
-		if (rc != EOF && fgets(str, 500, in) == NULL)
-			fprintf(stderr, "Read error or end of file.\n");
+	if (fgets(buf, sizeof(buf), in) == NULL)
+		return -1;
+
+	rc = sscanf(buf, "%llx %c %499s\n", &s->addr, &stype, str);
+	if (rc < 3) {
 		return -1;
 	}
 	if (strlen(str) > KSYM_NAME_LEN) {
@@ -162,6 +164,8 @@ static int read_symbol(FILE *in, struct sym_entry *s)
 		return -1;
 	/* exclude s390 kasan local symbols */
 	else if (!strncmp(sym, ".LASANPC", 8))
+		return -1;
+	else if (toupper(stype) == 'W' && strstr(sym, ".c") != NULL)
 		return -1;
 
 	/* include the type field in the symbol name, so that it gets
